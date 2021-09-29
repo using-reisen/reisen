99389e2b2b4ad480f3f7b23911e87ece7a2cc282
using-reisen <using.reisen@gmail.com>
2021-09-29 17:42:25 +0900

    using-reisen

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.search = new MetroFramework.Controls.MetroButton();
+            this.search_text = new MetroFramework.Controls.MetroTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.num = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.previous = new MetroFramework.Controls.MetroButton();
+            this.next = new MetroFramework.Controls.MetroButton();
+            this.down = new MetroFramework.Controls.MetroButton();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // search
+            // 
+            this.search.Location = new System.Drawing.Point(271, 43);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(43, 26);
+            this.search.TabIndex = 0;
+            this.search.Text = "검색";
+            this.search.Click += new System.EventHandler(this.search_Click);
+            // 
+            // search_text
+            // 
+            this.search_text.Location = new System.Drawing.Point(94, 43);
+            this.search_text.Name = "search_text";
+            this.search_text.Size = new System.Drawing.Size(158, 26);
+            this.search_text.TabIndex = 1;
+            this.search_text.KeyDown += new System.Windows.Forms.KeyEventHandler(this.search_text_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(127, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 12);
+            this.label1.TabIndex = 2;
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.num,
+            this.name});
+            this.dgv.Location = new System.Drawing.Point(13, 75);
+            this.dgv.Name = "dgv";
+            this.dgv.RowTemplate.Height = 23;
+            this.dgv.Size = new System.Drawing.Size(339, 194);
+            this.dgv.TabIndex = 3;
+            // 
+            // num
+            // 
+            this.num.HeaderText = "번호";
+            this.num.Name = "num";
+            this.num.Width = 55;
+            // 
+            // name
+            // 
+            this.name.HeaderText = "제목";
+            this.name.Name = "name";
+            this.name.Width = 280;
+            // 
+            // previous
+            // 
+            this.previous.Location = new System.Drawing.Point(13, 275);
+            this.previous.Name = "previous";
+            this.previous.Size = new System.Drawing.Size(75, 23);
+            this.previous.TabIndex = 4;
+            this.previous.Text = "이전";
+            this.previous.Click += new System.EventHandler(this.previous_Click);
+            // 
+            // next
+            // 
+            this.next.Location = new System.Drawing.Point(94, 275);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(75, 23);
+            this.next.TabIndex = 5;
+            this.next.Text = "다음";
+            this.next.Click += new System.EventHandler(this.next_Click);
+            // 
+            // down
+            // 
+            this.down.Location = new System.Drawing.Point(288, 275);
+            this.down.Name = "down";
+            this.down.Size = new System.Drawing.Size(64, 24);
+            this.down.TabIndex = 6;
+            this.down.Text = "다운";
+            this.down.Click += new System.EventHandler(this.down_Click);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.Location = new System.Drawing.Point(48, 45);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(40, 26);
+            this.metroLabel1.TabIndex = 7;
+            this.metroLabel1.Text = "제목";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "마나토끼 다운로더";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.BorderStyle = MetroFramework.Drawing.MetroBorderStyle.FixedSingle;
+            this.ClientSize = new System.Drawing.Size(365, 302);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.down);
+            this.Controls.Add(this.next);
+            this.Controls.Add(this.previous);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.search_text);
+            this.Controls.Add(this.search);
+            this.DisplayHeader = false;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(0, 30, 0, 0);
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Style = MetroFramework.MetroColorStyle.Blue;
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton search;
+        private MetroFramework.Controls.MetroTextBox search_text;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn num;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private MetroFramework.Controls.MetroButton previous;
+        private MetroFramework.Controls.MetroButton next;
+        private MetroFramework.Controls.MetroButton down;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+///selenium
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using ChromeDriverUpdater;
+using System.Threading;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : MetroForm
+    {
+        ChromeDriverService _chromeDriverService;
+        ChromeDriver _chromeDriver;
+        string Element = "/html/body/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/section/div[2]/form/div[1]/ul/li";
+        string localpath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+        //폴더명에 허용안되는 문자가 들어올시 변경되기 위해 선언
+        string manga_name2 = string.Empty;
+        string _name = string.Empty;
+        string _manga_name = string.Empty;
+        string code = string.Empty;
+
+        int _num = 4;
+        public Form1()
+        {
+            InitializeComponent();
+            dgv.RowHeadersVisible = false;
+            label1.Text = Text = @"reisen_downloder";
+            _chromeDriverService = ChromeDriverService.CreateDefaultService();
+            //바탕화면에 만화 폴더 생성
+            if (!Directory.Exists(localpath + "\\만화"))
+            {
+                Directory.CreateDirectory(localpath + "\\만화");
+            }
+        }
+        //검색
+        private void search_Click(object sender, EventArgs e)
+        {
+            search_function();
+        }
+
+        //enter Key로 검색
+        private void search_text_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == System.Windows.Forms.Keys.Enter)
+            {
+                search_function();
+            }
+        }
+
+        //검색결과
+        private void search_function()
+        {
+            try
+            {
+                var option = new ChromeOptions();
+                option.AddArgument("--headless");
+                _chromeDriverService.HideCommandPromptWindow = true;
+                _chromeDriver = new ChromeDriver(_chromeDriverService, option);
+                _chromeDriver.Navigate().GoToUrl("https://manatoki106.net/comic?stx=" + search_text.Text);
+                dgv_add();
+            }
+            catch { }
+
+        }
+
+        //데이터 추가
+        private void dgv_add()
+        {
+            dgv.Rows.Clear();
+            var headline = _chromeDriver.FindElementsByXPath(Element);
+            string name;
+            for (int i = 0; i < headline.Count; i++)
+            {
+                name = "/html/body/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/section/div[2]/form/div[1]/ul/li[" + (i + 1) + "]/div/div/div/div[1]/div/div/a/span";
+                var selenium_name = _chromeDriver.FindElementByXPath(name);
+                dgv.Rows.Add(i + 1, selenium_name.Text);
+            }
+        }
+        //다음 페이지
+        private void next_Click(object sender, EventArgs e)
+        {
+            _num++;
+            var headline = _chromeDriver.FindElementsByXPath(Element);
+            string page = "/html/body/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/section/div[2]/form/div[4]/ul/li[" + _num + "]/a";
+            var btn = _chromeDriver.FindElementByXPath(page);
+            btn.Click();
+            dgv_add();
+        }
+
+
+        //이전 페이지
+        private void previous_Click(object sender, EventArgs e)
+        {
+            _num--;
+            var headline = _chromeDriver.FindElementsByXPath(Element);
+            string page = "/html/body/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/section/div[2]/form/div[4]/ul/li[" + _num + "]/a";
+            var btn = _chromeDriver.FindElementByXPath(page);
+            btn.Click();
+            dgv_add();
+        }
+
+        //다운 버튼
+        private void down_Click(object sender, EventArgs e)
+        {
+            int manga_num = dgv.CurrentRow.Index + 1;
+            var headline = _chromeDriver.FindElementsByXPath(Element);
+            string page = "/html/body/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/section/div[2]/form/div[1]/ul/li[" + manga_num + "]/div/div/div/div[1]/div/a";
+            var manga = _chromeDriver.FindElementByXPath(page);
+            manga.Click();
+            kmdir();
+            manga_click();
+        }
+
+        //폴더 생성
+        private void kmdir()
+        {
+            int manga_num = dgv.CurrentRow.Index;
+            string manga_name = dgv.Rows[manga_num].Cells[1].Value.ToString();
+
+            //폴더명에  \ / : * ? " < > | 이 포함 되는지 확인
+            if (manga_name.Contains("\\"))
+                manga_name2 = manga_name.Replace("\\", "");
+            else if (manga_name.Contains("/"))
+                manga_name2 = manga_name.Replace("/", "");
+            else if (manga_name.Contains(":"))
+                manga_name2 = manga_name.Replace(":", "");
+            else if (manga_name.Contains("*"))
+                manga_name2 = manga_name.Replace("*", "");
+            else if (manga_name.Contains("?"))
+                manga_name2 = manga_name.Replace("?", "");
+            else if (manga_name.Contains("\""))
+                manga_name2 = manga_name.Replace("\"", "");
+            else if (manga_name.Contains("<"))
+                manga_name2 = manga_name.Replace("<", "");
+            else if (manga_name.Contains(">"))
+                manga_name2 = manga_name.Replace(">", "");
+            else if (manga_name.Contains("|"))
+                manga_name2 = manga_name.Replace("|", "");
+            else
+                manga_name2 = manga_name;
+
+
+            if (Directory.Exists(localpath + "\\만화" + "\\" + manga_name2) == false)
+            {
+                Directory.CreateDirectory(localpath + "\\만화\\" + manga_name2);
+            }
+        }
+        private void kmdir2(string name)
+        {
+            if (Directory.Exists(localpath + "\\만화" + "\\" + manga_name2 + "\\" + name) == false)
+            {
+                Directory.CreateDirectory(localpath + "\\만화\\" + manga_name2 + "\\" + name);
+            }
+        }
+
+        private void test_Click(object sender, EventArgs e)
+        {
+            down_load();
+        }
+
+
+        //화 클릭
+        private void manga_click()
+        {
+            Element = "/html/body/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/div[4]/section/article/form/div/ul/li";
+            var headline = _chromeDriver.FindElementsByXPath(Element);
+            var images = _chromeDriver.FindElements(By.TagName("a"));
+            //int dwon = headline.Count / 100;
+
+            for (int i = 0; i < images.Count; i++)
+            {
+                var rinkurl = images[i].GetAttribute(code);
+            }
+            for (int i = images.Count; i >= 0; i--)
+            {
+                try
+                {
+                    _name = "/html/body/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/div[4]/section/article/form/div/ul/li[" + (i + 1) + "]/div[2]/a";
+                    var manga_name = _chromeDriver.FindElementByXPath(_name);
+                    _manga_name = manga_name.Text.Substring(0, manga_name.Text.LastIndexOf('화'));
+                    kmdir2(_manga_name + "화");
+                    manga_name.Click();
+                    mangacode(i);
+                    down_load();
+                    //_chromeDriver.Navigate().Back();
+                }
+                catch { continue;}
+                //downloding.Value += dwon;
+            }
+            notifyIcon1.BalloonTipText = manga_name2 + "다운로드 완료";
+            notifyIcon1.BalloonTipTitle = "마나토끼 다운로더";
+            notifyIcon1.ShowBalloonTip(2);
+            _chromeDriver.Quit();
+            _chromeDriver.Close();
+            //downloding.Visible = false;
+        }
+
+        private void mangacode(int num)
+        {
+            var images2 = _chromeDriver.FindElements(By.TagName("option"));
+            
+            code = images2[num].GetAttribute("value");
+        }
+        private void down_load()
+        {
+            //downloding.Visible = true;
+            int j = 1;
+            var images = _chromeDriver.FindElements(By.TagName("img"));
+            var images2 = _chromeDriver.FindElements(By.TagName("option"));
+            string mangacode = string.Empty;
+            List<string> str2 = new List<string>();
+            string[] str = null;
+
+            for (int i = 0; i < images2.Count; i++)
+            {
+                var test = images2[0].GetAttribute("class");
+                mangacode = test;
+            }
+            
+            for (int i = 0; i < images.Count; i++)
+            {
+                try
+                {
+                    var imgurl = images[i].GetAttribute("data-" + mangacode);
+                    var imgname = images[i].GetAttribute("alt");
+
+                    WebClient daonloader = new WebClient();
+
+                    if (imgurl == null || (!imgurl.Contains(code) && !imgurl.Contains("type"))) continue;
+                    daonloader.DownloadFile(imgurl, localpath + "\\만화\\" + manga_name2 + "\\" + _manga_name + "화\\" + manga_name2 + "_" + j + ".jpg");
+                    j++;
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+            _chromeDriver.Navigate().Back();
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+
+            Updater updater = new Updater();
+            try
+            {
+                updater.Update(System.IO.Directory.GetCurrentDirectory() + @"\ChromeDriver.exe");
+            }
+            catch {  }
+        }
+    }
+}
